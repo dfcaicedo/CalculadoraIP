88c0f5f537a6a7c50d15fbf5a9125e26eca5e550
dcaic <dcaic@DFCAICEDO-PREDA>
2021-09-22 16:34:57 -0500

    Agregar archivos de proyecto.

---
+++ CalculadoraIP/Form1.Designer.cs
@@ -0,0 +1,320 @@
+﻿
+namespace CalculadoraIP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.textBox11 = new System.Windows.Forms.TextBox();
+            this.textBox12 = new System.Windows.Forms.TextBox();
+            this.textBox13 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textBox14 = new System.Windows.Forms.TextBox();
+            this.textBox15 = new System.Windows.Forms.TextBox();
+            this.textBox16 = new System.Windows.Forms.TextBox();
+            this.textBox17 = new System.Windows.Forms.TextBox();
+            this.textBox18 = new System.Windows.Forms.TextBox();
+            this.textBox19 = new System.Windows.Forms.TextBox();
+            this.textBox20 = new System.Windows.Forms.TextBox();
+            this.textBox21 = new System.Windows.Forms.TextBox();
+            this.textBox22 = new System.Windows.Forms.TextBox();
+            this.textBox23 = new System.Windows.Forms.TextBox();
+            this.textBox24 = new System.Windows.Forms.TextBox();
+            this.textBox25 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.textBox22);
+            this.groupBox1.Controls.Add(this.textBox23);
+            this.groupBox1.Controls.Add(this.textBox24);
+            this.groupBox1.Controls.Add(this.textBox25);
+            this.groupBox1.Controls.Add(this.numericUpDown1);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.textBox13);
+            this.groupBox1.Controls.Add(this.textBox12);
+            this.groupBox1.Controls.Add(this.textBox11);
+            this.groupBox1.Controls.Add(this.textBox10);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(621, 87);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Dirección IP";
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(7, 22);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(70, 22);
+            this.textBox10.TabIndex = 0;
+            this.textBox10.Text = "192";
+            this.textBox10.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox11
+            // 
+            this.textBox11.Location = new System.Drawing.Point(83, 22);
+            this.textBox11.Name = "textBox11";
+            this.textBox11.Size = new System.Drawing.Size(70, 22);
+            this.textBox11.TabIndex = 1;
+            this.textBox11.Text = "168";
+            this.textBox11.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox12
+            // 
+            this.textBox12.Location = new System.Drawing.Point(159, 22);
+            this.textBox12.Name = "textBox12";
+            this.textBox12.Size = new System.Drawing.Size(70, 22);
+            this.textBox12.TabIndex = 1;
+            this.textBox12.Text = "10";
+            this.textBox12.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox13
+            // 
+            this.textBox13.Location = new System.Drawing.Point(235, 21);
+            this.textBox13.Name = "textBox13";
+            this.textBox13.Size = new System.Drawing.Size(70, 22);
+            this.textBox13.TabIndex = 1;
+            this.textBox13.Text = "15";
+            this.textBox13.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(312, 26);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(12, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "/";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(330, 21);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            29,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDown1.TabIndex = 3;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            24,
+            0,
+            0,
+            0});
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.textBox18);
+            this.groupBox2.Controls.Add(this.textBox19);
+            this.groupBox2.Controls.Add(this.textBox20);
+            this.groupBox2.Controls.Add(this.textBox21);
+            this.groupBox2.Controls.Add(this.textBox14);
+            this.groupBox2.Controls.Add(this.textBox15);
+            this.groupBox2.Controls.Add(this.textBox16);
+            this.groupBox2.Controls.Add(this.textBox17);
+            this.groupBox2.Location = new System.Drawing.Point(12, 105);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(314, 104);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Mascara";
+            // 
+            // textBox14
+            // 
+            this.textBox14.Location = new System.Drawing.Point(235, 21);
+            this.textBox14.Name = "textBox14";
+            this.textBox14.ReadOnly = true;
+            this.textBox14.Size = new System.Drawing.Size(70, 22);
+            this.textBox14.TabIndex = 1;
+            // 
+            // textBox15
+            // 
+            this.textBox15.Location = new System.Drawing.Point(159, 22);
+            this.textBox15.Name = "textBox15";
+            this.textBox15.ReadOnly = true;
+            this.textBox15.Size = new System.Drawing.Size(70, 22);
+            this.textBox15.TabIndex = 1;
+            // 
+            // textBox16
+            // 
+            this.textBox16.Location = new System.Drawing.Point(83, 22);
+            this.textBox16.Name = "textBox16";
+            this.textBox16.ReadOnly = true;
+            this.textBox16.Size = new System.Drawing.Size(70, 22);
+            this.textBox16.TabIndex = 1;
+            // 
+            // textBox17
+            // 
+            this.textBox17.Location = new System.Drawing.Point(7, 22);
+            this.textBox17.Name = "textBox17";
+            this.textBox17.ReadOnly = true;
+            this.textBox17.Size = new System.Drawing.Size(70, 22);
+            this.textBox17.TabIndex = 0;
+            // 
+            // textBox18
+            // 
+            this.textBox18.Location = new System.Drawing.Point(236, 56);
+            this.textBox18.Name = "textBox18";
+            this.textBox18.ReadOnly = true;
+            this.textBox18.Size = new System.Drawing.Size(70, 22);
+            this.textBox18.TabIndex = 3;
+            // 
+            // textBox19
+            // 
+            this.textBox19.Location = new System.Drawing.Point(160, 57);
+            this.textBox19.Name = "textBox19";
+            this.textBox19.ReadOnly = true;
+            this.textBox19.Size = new System.Drawing.Size(70, 22);
+            this.textBox19.TabIndex = 4;
+            // 
+            // textBox20
+            // 
+            this.textBox20.Location = new System.Drawing.Point(84, 57);
+            this.textBox20.Name = "textBox20";
+            this.textBox20.ReadOnly = true;
+            this.textBox20.Size = new System.Drawing.Size(70, 22);
+            this.textBox20.TabIndex = 5;
+            // 
+            // textBox21
+            // 
+            this.textBox21.Location = new System.Drawing.Point(8, 57);
+            this.textBox21.Name = "textBox21";
+            this.textBox21.ReadOnly = true;
+            this.textBox21.Size = new System.Drawing.Size(70, 22);
+            this.textBox21.TabIndex = 2;
+            // 
+            // textBox22
+            // 
+            this.textBox22.Location = new System.Drawing.Point(236, 49);
+            this.textBox22.Name = "textBox22";
+            this.textBox22.ReadOnly = true;
+            this.textBox22.Size = new System.Drawing.Size(70, 22);
+            this.textBox22.TabIndex = 5;
+            this.textBox22.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox23
+            // 
+            this.textBox23.Location = new System.Drawing.Point(160, 50);
+            this.textBox23.Name = "textBox23";
+            this.textBox23.ReadOnly = true;
+            this.textBox23.Size = new System.Drawing.Size(70, 22);
+            this.textBox23.TabIndex = 6;
+            this.textBox23.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox24
+            // 
+            this.textBox24.Location = new System.Drawing.Point(84, 50);
+            this.textBox24.Name = "textBox24";
+            this.textBox24.ReadOnly = true;
+            this.textBox24.Size = new System.Drawing.Size(70, 22);
+            this.textBox24.TabIndex = 7;
+            this.textBox24.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox25
+            // 
+            this.textBox25.Location = new System.Drawing.Point(8, 50);
+            this.textBox25.Name = "textBox25";
+            this.textBox25.ReadOnly = true;
+            this.textBox25.Size = new System.Drawing.Size(70, 22);
+            this.textBox25.TabIndex = 4;
+            this.textBox25.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(466, 22);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Calcular";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(645, 644);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Calculadora IP";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox13;
+        private System.Windows.Forms.TextBox textBox12;
+        private System.Windows.Forms.TextBox textBox11;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox textBox18;
+        private System.Windows.Forms.TextBox textBox19;
+        private System.Windows.Forms.TextBox textBox20;
+        private System.Windows.Forms.TextBox textBox21;
+        private System.Windows.Forms.TextBox textBox14;
+        private System.Windows.Forms.TextBox textBox15;
+        private System.Windows.Forms.TextBox textBox16;
+        private System.Windows.Forms.TextBox textBox17;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox22;
+        private System.Windows.Forms.TextBox textBox23;
+        private System.Windows.Forms.TextBox textBox24;
+        private System.Windows.Forms.TextBox textBox25;
+    }
+}
+
---
+++ CalculadoraIP/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculadoraIP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public String DecimalaBinario(long decimales) // es como si recibiera el trapecio de entrada y la variables
+            //es decimales
+        {
+            String resp = "";
+            while (decimales > 0)
+            {
+                Int16 residuo;
+                residuo = Convert.ToInt16(decimales % 2);
+                resp = residuo.ToString() + resp;
+                decimales = (decimales - residuo) / 2;
+            }
+            while (resp.Length < 8)
+                resp = "0" + resp;
+            return resp;
+        }
+
+      
+        public String multi(String a,String b)
+        {
+            String resultado="";
+            while (a.Length > 0)
+            {
+                Int16 ultimoa;
+                Int16 ultimob;
+                ultimoa = Convert.ToInt16(a.Substring(a.Length - 1, 1));
+                ultimob = Convert.ToInt16(b.Substring(b.Length - 1, 1));
+                resultado = (ultimoa * ultimob).ToString() + resultado;
+                a = a.Substring(0, a.Length - 1); //Quitando el último carácter
+                b = b.Substring(0, b.Length - 1);
+            }
+            return resultado;
+        }
+        public string invertirnumero(String a)
+        {
+            a = a.Replace("1", "3");
+            a = a.Replace("0", "1");
+            a = a.Replace("3", "0");
+            return a;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            String n1 = this.textBox10.Text; // Trapecio
+            this.textBox25.Text = DecimalaBinario(Convert.ToInt64(n1));
+            String n2 = this.textBox11.Text;
+            this.textBox24.Text = DecimalaBinario(Convert.ToInt64(n2));
+            String n3 = this.textBox12.Text;
+            this.textBox23.Text = DecimalaBinario(Convert.ToInt64(n3));
+            String n4 = this.textBox13.Text;
+            this.textBox22.Text = DecimalaBinario(Convert.ToInt64(n4));
+            String mascara = "";
+            for (int a = 1; a <= numericUpDown1.Value; a++)
+            {
+                mascara += "1";
+            }
+            while (mascara.Length < 32)
+            {
+                mascara = mascara + "0";
+            }
+            this.textBox21.Text = mascara.Substring(0, 8);
+            this.textBox20.Text = mascara.Substring(8, 8);
+            this.textBox19.Text = mascara.Substring(16, 8);
+            this.textBox18.Text = mascara.Substring(24, 8);
+            textBox17.Text = BinarioaDecimal(textBox21.Text).ToString() ;
+            textBox16.Text = BinarioaDecimal(textBox20.Text).ToString();
+            textBox15.Text = BinarioaDecimal(textBox19.Text).ToString();
+            textBox14.Text = BinarioaDecimal(textBox18.Text).ToString();
+        }
+        public Int32 BinarioaDecimal(String valor)
+        {
+            int acu=0;
+            for (int a = 0; a < 8; a++)
+            {
+                int nume = Convert.ToInt32(valor.Substring((7 - a), 1));
+                acu = acu + nume * Convert.ToInt32(Math.Pow(2, a));
+            }
+            return acu;
+        }
+    }
+}
